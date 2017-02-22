0b971ec3c24fd0842aa32e5a8b993e02794b76f8
Kristin Caron <krissycaron@gmail.com>
2017-02-21 21:28:41 -0600

    consoled logged correct target for click event on the hidden id's

--- main.js
+++ main.js
@@ -6,38 +6,40 @@ var getPlanets = document.getElementById("showPlanets");
 var planetHolderDiv = document.getElementById("planetHolder");
 // console.log(getPLanetHolderDiv);  This prints to the console the div.
 
+// var planetBoxes = document.getElementByClassName('planetBox'); something wrong here
+
 var planets = [
     {
         name:"Mercury",
-        url: ""
+        url: "http://science.jrank.org/kids/article_images/space_p20.jpg"
     },
     {
         name:"Venus",
-        url: ""
+        url: "https://upload.wikimedia.org/wikipedia/commons/8/85/Venus_globe.jpg"
     },
     {
         name:"Earth",
-        url: ""
+        url: "https://semestahidayah.files.wordpress.com/2010/12/1600_1231.jpg"
     },
     {
         name:"Mars",
-        url: ""
+        url: "http://skywalker.cochise.edu/wellerr/students/mars-ice/project_files/image002.jpg"
     },
     {
         name:"Jupiter",
-        url: ""
+        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/Hubble_Captures_Vivid_Auroras_in_Jupiter's_Atmosphere.jpg/220px-Hubble_Captures_Vivid_Auroras_in_Jupiter's_Atmosphere.jpg"
     },
     {
         name:"Saturn",
-        url: ""
+        url: "http://momentcar.com/images/saturn-vue-3.jpg"
     },
     {
         name:"Uranus",
-        url: ""
+        url: "https://upload.wikimedia.org/wikipedia/commons/3/3d/Uranus2.jpg"
     },
     {
         name:"Neptune",
-        url: ""
+        url: "http://www.crystalinks.com/neptune-rings.jpg"
     }
 ];
 //array of objects not strings. this is an array for 8 objects, 
@@ -62,15 +64,29 @@ function writeToDom (){
 		//String Templeting "ticking"
 		var newPlanet = "";
 		newPlanet+= `<div class="planetbox", id="planetbox-${i}">` // i is a variable so it needs to be wrapped in {}
-		newPlanet += `<div class="planetName"> ${planets[i].name} </div>`; // as long as you wrap the entire HTML with "tick" marks 
+		newPlanet+= `<div class="planetName hidden"> ${planets[i].name} </div>`; // as long as you wrap the entire HTML with "tick" marks 
 		//this will create a "box" div for each planet object
+		newPlanet+= `<img class="planetImage" src="${planets[i].url}">`
 		newPlanet+=`</div>`
 		planetHolderDiv.innerHTML += newPlanet;
 	}
 }	
 
 
-getPlanets.addEventListener("click", writeToDom);
+function showMeTheName(event){
+	if(event.target.className === 'planetImage'){ //selecting the specific target
+		console.log("event worked", event);
+		console.log("unique ID", event.target.parentNode.id);
+		console.log("worked", event.target.previousSibling); 
+	}
+}
+
+getPlanets.addEventListener("mouseenter", writeToDom); //hover with mouse to reveal
+
+// console.log("planetBoxes", planetBoxes);
+// planetBoxes[0].addEventListener("click", showMeTheName); //event listeners start on page load
+
+document.body.addEventListener("click", showMeTheName);
 
 // function button (){
 // 	document.getElementById('').innerHTML
